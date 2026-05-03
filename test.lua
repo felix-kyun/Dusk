90d18992792109fa290f7165188c0b22fb406462
Praise Jacob <iampraisejacob@gmail.com>
2026-05-04 02:54:23 +0530

    test: update tests to use code values through metatable instead of hardcoding escape codes

--- test.lua
+++ test.lua
@@ -1,9 +1,32 @@
-local d = require("Dusk")
+local d = require("dusk")
+local codes = getmetatable(d)._codes
 
 -- multi chaining
-assert(d.red.bold("test") == "\27[31m\27[1mtest\27[22m\27[39m", "multi chaining")
+assert(d.red.bold("test") == ""
+	.. codes.red.open
+	.. codes.bold.open
+	.. "test"
+	.. codes.bold.close
+	.. codes.red.close,
+	"multi chaining")
 
 -- chain reuse
 local redBold = d.red.bold
-assert(redBold.underline("test") == "\27[31m\27[1m\27[4mtest\27[24m\27[22m\27[39m", "chain reuse underline")
-assert(redBold("test") == "\27[31m\27[1mtest\27[22m\27[39m", "chain reuse")
+assert(redBold.strikethrough("test") == ""
+	.. codes.red.open
+	.. codes.bold.open
+	.. codes.strikethrough.open
+	.. "test"
+	.. codes.strikethrough.close
+	.. codes.bold.close
+	.. codes.red.close,
+	"chain reuse strikethrough")
+assert(redBold.underline("test") == ""
+	.. codes.red.open
+	.. codes.bold.open
+	.. codes.underline.open
+	.. "test"
+	.. codes.underline.close
+	.. codes.bold.close
+	.. codes.red.close,
+	"chain reuse")
